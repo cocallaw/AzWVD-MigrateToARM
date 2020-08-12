725efbf4a321ed16f75f1ea896208cfb8db99e06
cocallaw <coreylcallaway@gmail.com>
2020-08-12 15:24:03 -0400

    Add Installation and Unsintall RDInfra Object

--- Migrate-WVDHostToARM.ps1
+++ Migrate-WVDHostToARM.ps1
@@ -49,6 +49,18 @@ function Write-Log {
     } 
 }
 
+function Install-AzModule {
+    Install-PackageProvider NuGet -Force
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    try {
+        Install-Module -Name Az -AllowClobber -Scope CurrentUser
+        Write-Log -Message "Installed Az PowerShell modules successfully"
+    }
+    catch {
+        Write-Log -Message "Unable to install Az PowerShell modules successfully"
+    }    
+}
+
 function Load-Module ($m) {
 
     # If module is imported say that and do nothing
@@ -81,8 +93,11 @@ function Load-Module ($m) {
 $startDTM = (Get-Date)
 Write-Log -Message "Starting WVD Migration on Host"
 
-#Check for Prerequsites
+#Collect VM Info
+$hostVMname = $env:computername
+$hostVMdomain = $env:USERDNSDOMAIN
 
+#Check for Prerequsites
 
 $WVDMigrateBasePath = "c:\WVDMigrate\"
 $WVDMigrateLogPath = "c:\WVDMigrate\logs"
@@ -90,20 +105,36 @@ $WVDMigrateInfraPath = "C:\WVDMigrate\Infra"
 $infraURI = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
 
 #Creating Directory Structure and Downloading Assets
-New-Item -Path $WVDMigrateLogPath -ItemType Directory -Force
-New-Item -Path $WVDMigrateInfraPath -ItemType Directory -Force
-Write-Log -Message "Created Directory Structure for Assets and Logging"
+try {
+    New-Item -Path $WVDMigrateLogPath -ItemType Directory -Force
+    New-Item -Path $WVDMigrateInfraPath -ItemType Directory -Force
+    Write-Log -Message "Created Directory Structure for Assets and Logging"
+}
+catch {
+    Write-Log -Message "Unable to Create Directory Structure for Assets and Logging"
+}
 
+
+#Download Current Version of WVD Agent 
 $AssetstartDTM = (Get-Date)
 Invoke-WebRequest -Uri $infraURI -OutFile "$WVDMigrateInfraPath\Microsoft.RDInfra.RDAgent.Installer-x64.msi"
 Write-Log -Message "Downloaded RDInfra Agent"
 $AssetendDTM = (Get-Date)
 Write-Log -Message "Asset Download Time: $(($AssetendDTM-$AssetstartDTM).totalseconds) seconds"
 
- 
-
-#Collect VM Info
-
-#Download Current Version of WVD Agent 
-
 #Remove Installed versions of WVD Agent 
+Write-Log -Message "Uninstalling any previous versions of RDInfra Agent on VM"
+$legacy_agent_uninstall_status = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {5389488F-551D-4965-9383-E91F27A9F217}", "/quiet", "/qn", "/norestart", "/passive", "/l* $WVDDeployLogPath\AgentUninstall.txt" -Wait -Passthru
+$sts = $legacy_agent_uninstall_status.ExitCode
+#Remove previous versions of RDInfraAgent DLLs
+Write-Log -Message "Uninstalling any previous versions of RDInfra Agent DLL on VM"
+$agent_uninstall_status = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {CB1B8450-4A67-4628-93D3-907DE29BF78C}", "/quiet", "/qn", "/norestart", "/passive", "/l* $WVDDeployLogPath\AgentUninstall.txt" -Wait -Passthru
+$sts = $agent_uninstall_status.ExitCode
+
+#install New WVD Agent
+$AgentInstaller = (dir $WVDMigrateInfraPath\ -Filter *.msi | Select-Object).FullName
+Write-Log -Message "Starting install of $AgentInstaller"
+$agent_deploy_status = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $AgentInstaller", "/quiet", "/qn", "/norestart", "/passive", "REGISTRATIONTOKEN=$RegistrationToken", "/l* $WVDDeployLogPath\AgentInstall.txt" -Wait -Passthru
+$sts = $agent_deploy_status.ExitCode
+Write-Log -Message "Installing RD Infra Agent on VM Complete. Exit code=$sts"
+
