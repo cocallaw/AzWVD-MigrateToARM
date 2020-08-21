c063c2658fcc43e88e9090917b3b6ca850a44b79
cocallaw <coreylcallaway@gmail.com>
2020-08-21 15:16:54 -0400

    Create Run-WVDHostOperations.ps1

---
+++ VMScripts/Run-WVDHostOperations.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(mandatory = $true)]
+    [string]$HostPoolToken,
+
+    [Parameter(mandatory = $false)]
+    [switch]$PreStageOnly,
+
+    [Parameter(mandatory = $false)]
+    [switch]$UpdateOnly,
+
+    [Parameter(mandatory = $false)]
+    [switch]$FullMigration
+)
+
+$WVDMigrateInfraPath = "C:\WVDMigrate"
+$infraURI = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
+
+if (($PreStageOnly) -or ($FullMigration)) {
+    #Create folder for agent download
+    try {
+        New-Item -Path $WVDMigrateInfraPath -ItemType Directory -Force
+        Write-Host "Created Directory Structure for WVD Agent"
+    }
+    catch {
+        Write-Host "Unable to Create Directory Structure WVD Agent"
+    }
+
+    #Download Current Version of WVD Agent 
+    $AssetstartDTM = (Get-Date)
+    Invoke-WebRequest -Uri $infraURI -OutFile "$WVDMigrateInfraPath\Microsoft.RDInfra.RDAgent.Installer-x64.msi"
+    Write-Host "Downloaded RDInfra Agent"
+    $AssetendDTM = (Get-Date)
+    Write-Host "Agent Download Time: $(($AssetendDTM-$AssetstartDTM).totalseconds) seconds"
+}
+
+if (($UpdateOnly) -or ($FullMigration)) {
+    
+}
