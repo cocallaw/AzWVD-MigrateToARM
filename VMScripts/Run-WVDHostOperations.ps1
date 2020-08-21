4937c6818fb39cf13157073aec82b1558240a458
cocallaw <coreylcallaway@gmail.com>
2020-08-21 15:34:20 -0400

    Add Agent Update Logic

--- VMScripts/Run-WVDHostOperations.ps1
+++ VMScripts/Run-WVDHostOperations.ps1
@@ -34,5 +34,18 @@ if (($PreStageOnly) -or ($FullMigration)) {
 }
 
 if (($UpdateOnly) -or ($FullMigration)) {
-    
+    #Remove Installed versions of WVD Agent 
+    Write-Host "Uninstalling any previous versions of RDInfra Agent on VM"
+    $RDInfraApps = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Remote Desktop Services Infrastructure Agent" }
+    foreach ($app in $RDInfraApps) {
+        Write-Host "Uninstalling Infra Agent $app.Version"
+        $app.Uninstall()
+    }
+
+    $AgentInstaller = (dir $WVDMigrateInfraPath\ -Filter *.msi | Select-Object).FullName
+    $RegistrationToken = $HostPoolToken
+    Write-Host "Starting install of $AgentInstaller"
+    $agent_deploy_status = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $AgentInstaller", "/quiet", "/qn", "/norestart", "/passive", "REGISTRATIONTOKEN=$RegistrationToken", "/l* $WVDMigrateInfraPath\AgentInstall.txt" -Wait -Passthru
+    $sts = $agent_deploy_status.ExitCode
+    Write-Host "Installing RD Infra Agent on VM Complete. Exit code=$sts"
 }
